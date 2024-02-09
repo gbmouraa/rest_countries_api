79b373dc5fa3b8d022d2fa839fa02869aef5ab83
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-08 22:06:35 -0300

    style: add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyle from "../src/styles/global";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  html{
+    // isso torna a utilização da unidade de medida rem mais fácil
+    // ex: 30px = 3rem; 32px = 3.2rem
+    font-size: 62.5%;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    width: 100%;
+    min-height: 100vh;
+    font-size: 1.6rem;
+  }
+
+  body,input,button{
+    font-family: 'Nunito Sans', sans-serif;
+  }
+
+  button{
+    border: none;
+    outline: none;
+    cursor: pointer;
+    transition: all .7s ease;
+  }
+
+  .App{
+    width: 100%;
+  }
+`;
