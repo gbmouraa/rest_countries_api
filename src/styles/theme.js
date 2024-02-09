bd38993da15f3de556c3381ab36aafcb0a79065d
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-08 22:06:49 -0300

    style: theme created

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export const darkTheme = {
+  element_bg: "#2b3945",
+  body_bg: "#202c37",
+  text: "#ffffff",
+};
+
+export const lightTheme = {
+  element_bg: "#ffffff",
+  body_bg: "#202c37",
+  text: "#111517",
+  input: "#858585",
+};
