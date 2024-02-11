ef4bb799518a00aa5b40ca408a63b790d59cca8d
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-10 21:21:33 -0300

    feat: assigned thema value based on localStorage

--- src/context/restCountries.js
+++ src/context/restCountries.js
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const RestCountriesContext = createContext({});
 
 export function RestCountriesProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("@restCountries") || "light";
+  });
 
   return (
     <RestCountriesContext.Provider value={{ theme, setTheme }}>
