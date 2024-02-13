6d70f23d14e176a69209912f9b2a4174c9bc5d62
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-13 17:37:06 -0300

    feat: created home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import InputGroup from "../components/InputGroup";
+import Countries from "../components/Countries";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <InputGroup />
+      <Countries />
+    </>
+  );
+}
