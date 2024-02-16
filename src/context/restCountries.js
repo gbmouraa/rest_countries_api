5492f4faa48f9b5ad479f680e0d8869174c3ac47
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-15 21:09:10 -0300

    feat: add fetchCountries fn

--- src/context/restCountries.js
+++ src/context/restCountries.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { api } from "../services/api";
 
 export const RestCountriesContext = createContext({});
 
@@ -7,8 +8,27 @@ export function RestCountriesProvider({ children }) {
     return localStorage.getItem("@restCountries") || "light";
   });
 
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  async function fetchCountries(query) {
+    try {
+      const fetchData = await api.get(`/${query}`);
+      const response = fetchData.data;
+      setCountries(response);
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  }
+
   return (
-    <RestCountriesContext.Provider value={{ theme, setTheme }}>
+    <RestCountriesContext.Provider
+      value={{ theme, setTheme, countries, loading, fetchCountries, error }}
+    >
       {children}
     </RestCountriesContext.Provider>
   );
