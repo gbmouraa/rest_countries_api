bc9690b736e7ff1a132c804b077cdb4c8c4e002f
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-16 21:57:28 -0300

    feat: add coutry to routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
+import CountryInfo from "../Pages/CoutryInfo";
 
 export default function AppRouter() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/country/:coutryName" element={<CountryInfo />} />
       </Routes>
     </div>
   );
