6074f325b8c919193e9f80f0de073039975c6527
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-21 21:07:32 -0300

    refactor: rm unnecessary import

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { RestCountriesContext } from "../../context/restCountries";
 import {
   CountriesContainer,
