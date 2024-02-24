dbd3ffe7af92f2aa5a774f7280831765db31bc18
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-23 21:42:28 -0300

    chore: add param optional param to Home url

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ export default function AppRouter() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/:page?" element={<Home />} />
       </Routes>
       <Routes>
         <Route path="/coutry/:coutryName" element={<CoutryPage />} />
