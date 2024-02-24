d683754e3f16fe113b3b5c31f792558097fff091
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-24 16:42:28 -0300

    feat: add navigation to first page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { RestCountriesContext } from "../../context/restCountries";
 import { HeaderWrapper, ThemeSwitcher, HeaderContainer } from "./Header.style";
 import { Title } from "../Title.style";
@@ -6,7 +7,9 @@ import { FiMoon } from "react-icons/fi";
 import { IoSunnyOutline } from "react-icons/io5";
 
 export default function Header() {
-  const { theme, setTheme } = useContext(RestCountriesContext);
+  const { theme, setTheme, setCurrentPage } = useContext(RestCountriesContext);
+
+  const navigate = useNavigate();
 
   function toggleTheme() {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
@@ -16,10 +19,20 @@ export default function Header() {
     localStorage.setItem("@restCountries", theme);
   }, [theme]);
 
+  function handleNavigation() {
+    setCurrentPage(1);
+    navigate("/1");
+  }
+
   return (
     <HeaderContainer>
       <HeaderWrapper>
-        <Title size="2" className="header-title">
+        <Title
+          size="2"
+          className="header-title"
+          onClick={() => handleNavigation()}
+          style={{ cursor: "pointer" }}
+        >
           Where in the world?
         </Title>
         <ThemeSwitcher onClick={toggleTheme}>
